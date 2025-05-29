00c16249dc85daed8b373e08b05136425ebbd496
abdoantr <36226860+abdoantr@users.noreply.github.com>
2025-05-29 18:17:13 +0300

    Create migration.ps1

---
+++ migration.ps1
@@ -0,0 +1,12 @@
+py .\manage.py migrate
+py .\manage.py migrate accounts
+py .\manage.py migrate appointments
+py .\manage.py migrate kb
+py .\manage.py migrate payments
+py .\manage.py migrate services
+py .\manage.py migrate tickets
+py .\manage.py showmigrations
+py .\manage.py create_sample_services
+py .\manage.py loaddata
+py .\manage.py runserver
+
